383009b11f37c752108bdd12a704534ac5023259
Ivan Novick <ivannovick@users.noreply.github.com>
2020-02-04 09:36:28 -0800

    Create getonetweet.py

---
+++ pyclient/getonetweet.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+try:
+    # You can also explicitly pass parameters like username, password,etc
+    # this code assumes PGHOST, PGPASSWORD, PGUSER are set in environment
+    connection = psycopg2.connect(database = "twitter")
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * from tweets")
+
+    record = cursor.fetchone()
+    print("VERSION: ", record,"\n")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
