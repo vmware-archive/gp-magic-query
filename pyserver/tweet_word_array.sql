240adf17758dd9051f1333b5fe104664aee8b6a5
Ivan Novick <ivannovick@users.noreply.github.com>
2020-02-05 10:20:43 -0800

    Create tweet_word_array.sql

---
+++ pyserver/tweet_word_array.sql
@@ -0,0 +1,14 @@
+DROP FUNCTION IF EXISTS tweet_word_array(text);
+CREATE FUNCTION tweet_word_array(tweet text)
+  RETURNS text[]
+AS $$
+  if tweet is None:
+      return []
+  return tweet.split()
+$$ LANGUAGE plpythonu;
+
+\x
+
+SELECT full_text, tweet_word_array(full_text)
+FROM tweets where full_text is not null
+LIMIT 5;
