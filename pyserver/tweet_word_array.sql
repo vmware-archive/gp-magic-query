0ca4455f1835c14706c0c63d87b7f379562cbe13
Ivan Novick <ivannovick@users.noreply.github.com>
2020-02-05 10:30:49 -0800

    Update tweet_word_array.sql

--- pyserver/tweet_word_array.sql
+++ pyserver/tweet_word_array.sql
@@ -9,7 +9,7 @@ $$ LANGUAGE plpythonu;
 
 \x
 
+CREATE TABLE tweet_word_array_result AS
 SELECT full_text, tweet_word_array(full_text)
 FROM tweets where full_text is not null
-AND lang = 'en'
-LIMIT 5;
+AND lang = 'en';
